9a9a576f01d91a24407199ed512213ef5a8ea00e
EngKiat-20 <82920974+EngKiat-20@users.noreply.github.com>
2021-08-04 11:49:35 +0800

    Delete database directory

--- database/sql_init.sql
+++
@@ -1,9 +0,0 @@
-DROP DATABASE IF EXISTS ades_ca3_p2020952;
-CREATE DATABASE ades_ca3_p2020952;
-USE ades_ca3_p2020952;
-
-CREATE TABLE player_data(
-	entry_id int not null primary key auto_increment,
-	username varchar(255),
-    score int not null
-);
