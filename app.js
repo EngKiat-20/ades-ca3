11394de8f6d02bc605db63c670673cd63303f55c
EngKiat-20 <82920974+EngKiat-20@users.noreply.github.com>
2021-08-14 09:04:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ const sessions = {};
 const playerName = {};
 const playerData = {};
 
+/*Heroku-exclusive code here*/
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+/*Heroku-exclusive code ends here*/
+
 app.post('/session', function (req, res, next) {
     const sessionId = nanoid();
     sessions[sessionId] = 1;
